d531322a3edc256c09d16e4689c475e2f1f3c2bb
Matthew Zikherman <matt@bestvendor.com>
2016-05-29 11:51:13 -0400

    ugh

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  skip_before_action :verify_authenticity_token
   http_basic_authenticate_with name: QebaParking.config[:basic_auth_key], password: QebaParking.config[:basic_auth_password]
 end
