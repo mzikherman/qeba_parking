0ad808ac14d00028be91a6508cda17d36929b8d0
Matthew Zikherman <matt@bestvendor.com>
2016-05-29 10:38:29 -0400

    eh

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
   http_basic_authenticate_with name: QebaParking.config[:basic_auth_key], password: QebaParking.config[:basic_auth_password]
 end
